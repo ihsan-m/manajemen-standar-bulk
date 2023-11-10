1d197718f8d9e4cf6003318614f77a29ca67cf42
muhammad.hilmi02 <muhammad.hilmi02@ui.ac.id>
2023-11-10 08:37:19 +0700

    feat: added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Manajemen Standar Bulk',
-  description: '',
-}
+  title: "Manajemen Standar Bulk",
+  description: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen font-sans transition-colors`}>
-        {children}</body>
+      <head>
+        <link rel="icon" type="image/svg+xml" href="../public/SB Logo.svg" />
+      </head>
+      <body
+        className={`${inter.className} min-h-screen font-sans transition-colors`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
 
 // test commit
